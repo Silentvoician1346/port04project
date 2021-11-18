fd0f5308329a2306c55eb9faed70de078475b564
Silentvoician1346 <silentvoician1346@gmail.com>
2021-11-18 10:41:17 +0700

    after deploy rev2 contact

--- pages/contactsimple.js
+++ pages/contactsimple.js
@@ -3,25 +3,25 @@
 export default function ContactSimple() {
     return(
         <div class="w-full h-full">
-            <div class="flex w-full h-[100px] bg-[#07121e] mx-[0px] sm:mx-[10px] my-[20px] justify-center">
+            <div class="flex flex-1 w-full h-[100px] bg-[#07121e] mx-[0px] sm:mx-[10px] my-[20px] justify-center">
                 <div class="w-[84px] h-[48px] px-[10px] my-auto">
-                    <text class="flex text-white font-bold justify-center pb-[2px]">BCA</text>
+                    <text class="flex text-white font-bold text-xs justify-center pb-[2px]">BCA</text>
                     <img src="/image/online1.gif" />
-                </div>
+                </div>r
                 <div class="w-[84px] h-[48px] px-[10px] my-auto">
-                    <text class="flex text-white font-bold justify-center pb-[2px]">MANDIRI</text>
+                    <text class="flex text-white font-bold text-[10px] justify-center pb-[2px]">MANDIRI</text>
                     <img class="w-[100px]" src="/image/online1.gif" />
                 </div>
                 <div class="w-[84px] h-[48px] px-[10px] my-auto">
-                    <text class="flex text-white font-bold justify-center pb-[2px]">BNI</text>
+                    <text class="flex text-white font-bold text-xs justify-center pb-[2px]">BNI</text>
                     <img src="/image/online1.gif" />
                 </div>
                 <div class="w-[84px] h-[48px] px-[10px] my-auto">
-                    <text class="flex text-white font-bold justify-center pb-[2px]">BRI</text>
+                    <text class="flex text-white font-bold text-xs justify-center pb-[2px]">BRI</text>
                     <img src="/image/online1.gif" />
                 </div>
                 <div class="w-[84px] h-[48px] px-[10px] my-auto">
-                    <text class="flex text-white font-bold justify-center pb-[2px]">DANAMON</text>
+                    <text class="flex text-white font-bold text-[10px] justify-center pb-[2px]">DANAMON</text>
                     <img src="/image/online1.gif" />
                 </div>
                 <div class="w-0 sm:w-[1px] h-[48px] mx-[0px] sm:mx-[10px] my-auto bg-blue-200 "/>
