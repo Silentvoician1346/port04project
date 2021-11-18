2389b816b2d18be9ae23a4bce4d216a3167bcdc6
Silentvoician1346 <silentvoician1346@gmail.com>
2021-11-18 10:33:01 +0700

    after deploy rev02

--- pages/content.js
+++ pages/content.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Content() {
     return(
-        <div class="grid grid-cols-2 sm:grid-cols-4 w-screen h-full mb-[15px]">
+        <div class="grid grid-cols-2 sm:grid-cols-4 w-full h-full mb-[15px]">
             <Link href="/daftar">
                 <button class="w-[160px] sm:w-[226px] h-[80px] sm:h-[110px] mt-[10px] mx-auto bg-red-100" >
                     <img src="/image/content1.jpg" />
