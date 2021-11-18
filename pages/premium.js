2da0e658ba7212ef9400f122852a90420f65ba64
Silentvoician1346 <silentvoician1346@gmail.com>
2021-11-18 19:58:58 +0700

    rev 2 marquee btm

--- pages/premium.js
+++ pages/premium.js
@@ -11,13 +11,7 @@ import Diamonds from './diamonds';
 export default function Premium() {
   return (
     <div class="container w-screen sm:max-w-full h-full bg-background1 bg-center bg-fixed">
-      {/* MARQUEE */}
-      <Marquee pauseOnHover='true' gradient={false}>
-        <div class="w-full h-[75px] sm:h-[35px] bg-black text-yellow-300 text-2xl sm:text-xl pt-[22px] sm:pt-[0px]">
-          PangYa was an online multiplayer casual golf simulation game designed by Korean development company Ntreev Soft and NCSoft. From 2005 until March 2009, the game was known as Albatross18 in North America.
-        </div>
-      </Marquee>
-      <div ></div>
+      
       <div class="w-screen sm:w-[1054px] mx-auto px-[2px] sm:px-[15px] h-full bg-gradient-to-r from-blue-900 via-blue-500 to-blue-900 relative z-10 justify-center">
       {/* HEADER */}
       <Header />
@@ -28,6 +22,12 @@ export default function Premium() {
       {/* FOOTER */}
       <Footer />
       </div>
+      {/* MARQUEE */}
+      <Marquee pauseOnHover='true' gradient={false}>
+        <div class="w-full h-[75px] sm:h-[35px] bg-black text-yellow-300 text-2xl sm:text-xl pt-[22px] sm:pt-[0px]">
+          PangYa was an online multiplayer casual golf simulation game designed by Korean development company Ntreev Soft and NCSoft. From 2005 until March 2009, the game was known as Albatross18 in North America.
+        </div>
+      </Marquee>
     </div>
   )
 }
